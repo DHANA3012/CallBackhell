a2b5461b0d3c161ccc8fa9e6156d9bbcf410b4c1
DHANA3012 <109264553+DHANA3012@users.noreply.github.com>
2023-01-12 21:31:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+let task= document.createElement('h1')
+setTimeout(()=>{
+    task.innerHTML='10'
+    task.style.color='red'
+    console.log('10')    
+    document.body.append(task)
+setTimeout(()=>{
+    console.log('9')
+    task.innerHTML='9'
+    document.body.append(task)
+setTimeout(()=>{
+    console.log('8')
+    task.innerHTML='8'
+    document.body.append(task)
+setTimeout(()=>{
+    console.log('7')
+    task.innerHTML='7'
+    document.body.append(task)
+setTimeout(()=>{
+    console.log('6')
+    task.innerHTML='6'
+    document.body.append(task)
+setTimeout(()=>{
+    console.log('5')
+    task.innerHTML='5'
+    document.body.append(task)
+setTimeout(()=>{
+    console.log('4')
+    task.innerHTML='4'
+    document.body.append(task)
+setTimeout(()=>{
+    console.log('3')
+    task.innerHTML='3'
+    document.body.append(task)
+setTimeout(()=>{
+    console.log('2')
+    task.innerHTML='2'
+    document.body.append(task)
+
+setTimeout(()=>{
+    console.log('1')
+    task.innerHTML='1'
+    document.body.append(task)
+
+setTimeout(()=>{
+        task.style.color='green'
+        console.log('Happy Independence Day!!!')
+        task.innerHTML='Happy Independence Day!!!'
+        document.body.append(task)     
+},1000)         
+},1000)        
+},1000)
+},1000)               
+},1000)
+},1000)   
+},1000)
+},1000)
+},1000) 
+},1000) 
+},1000) 
+
